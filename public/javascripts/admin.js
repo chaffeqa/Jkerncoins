cf9bd50d70bf3c1a59b35b1486b88dc6c4ad5259
Quinn <chaffeqa@gmail.com>
2011-12-29 00:51:38 -0500

    fixed error on not striping titles

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,44 +1,44 @@
 $(function() {
-	
-	// Max width your labels
-	var max = 0;
-	$("label").each(function(){
-		if ($(this).width() > max)
-		    max = $(this).width();
-	    });
-	$("label").width(max+10);
-	
-	// Function for forms with inline hints
-	$('input[title]').each(function() {
-		if($(this).val() === '') {
-		    $(this).val($(this).attr('title'));	
-		}
-		
-		$(this).focus(function() {
-			if($(this).val() == $(this).attr('title')) {
-			    $(this).val('').addClass('focused');	
-			}
-		    });
-		$(this).blur(function() {
-			if($(this).val() === '') {
-			    $(this).val($(this).attr('title')).removeClass('focused');
-			}
-		    });
-	    });
 
-	// Hint Expander
-	$(".expander").click(function() {
-		$(this).parent().siblings(".expanded-hint").toggle(200, function(){});
-	    });
-	$(".expanded-hint").hide(); // Hide initially
+  // Max width your labels
+  var max = 0;
+  $("label").each(function(){
+    if ($(this).width() > max)
+        max = $(this).width();
+      });
+  $("label").width(max+10);
+
+  // Function for forms with inline hints
+  $('input[title]').each(function() {
+    if($(this).val() === '') {
+        $(this).val($(this).attr('title'));
+    }
+
+    $(this).focus(function() {
+      if($(this).val() == $(this).attr('title')) {
+          $(this).val('').addClass('focused');
+      }
+        });
+    $(this).blur(function() {
+      if($(this).val() === '') {
+          $(this).val($(this).attr('title')).removeClass('focused');
+      }
+        });
+      });
+
+  // Hint Expander
+  $(".expander").click(function() {
+    $(this).parent().siblings(".expanded-hint").toggle(200, function(){});
+      });
+  $(".expanded-hint").hide(); // Hide initially
     });
 
-function stripTitles() {
+  function stripTitles() {
     $('input[title]').each(function() {
-	    if($(this).val() == $(this).attr('title')) {
-		$(this).val('');
-	    };
-	});
+      if($(this).val() == $(this).attr('title')) {
+    $(this).val('');
+      };
+  });
 }
 
 
